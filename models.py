36725873d1fcb731d896b4279a649d186ad73c8c
ISHAAN KAMRA <ishaan1091@gmail.com>
2020-04-19 11:54:55 +0530

    changed comments to test

--- models.py
+++ models.py
@@ -55,7 +55,7 @@ class Projects(db.Model):
     description = db.Column(db.String(120), nullable=False)
 
 
-class New(db.model):
+class New(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
